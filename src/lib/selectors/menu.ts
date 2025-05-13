626f2cf2d5af6be9c0834529e66666dcdf2bd6be
TECHNIFI <manahiladil137@gmail.com>
2025-05-13 17:09:11 +0500

    Describe what you changed

--- src/lib/selectors/menu.ts
+++ src/lib/selectors/menu.ts
@@ -1,11 +1,13 @@
 export const selectors={
     elementsCard: "//div[@class='card mt-4 top-card']//div[@class='card-body']/h5[text()='Elements']",
-    menu:'//div[@class="element-group"][1]',
+    menu:'//div[@class="element-group"][4]',
     Selectmenu: "//span[text()='Select Menu']",
     selectvalue:'(//div[@class=" css-1hwfws3"])[1]',
     selectone:'(//div[@class=" css-1hwfws3"])[2]',
     oldstyleselectmenu:'//select[@id="oldSelectMenu"]',
     multiplemenu:'//div[contains(@class, "css-1wa3eu0-placeholder") and text()="Select..."]',
     standart:'//select[@id="cars"]',
+    selectblue:'//div[contains(@id, "react-select") and text()="Blue"]',
+    selectgreen:'//div[contains(@id, "react-select") and text()="Blue"]',
     
 }
--- src/tests/menuselect.spec.ts
+++ src/tests/menuselect.spec.ts
@@ -3,22 +3,36 @@ import { selectors } from "../lib/selectors/menu";
 
 export async function clickAllDropdowns(page: Page): Promise<void> {
   await page.locator(selectors.elementsCard).click();
-  await page.locator(selectors.menu).scrollIntoViewIfNeeded();
+  await page.locator(selectors.menu).scrollIntoViewIfNeeded({ timeout: 10000 });
   await page.locator(selectors.menu).click();
+  
+  await page.locator(selectors.Selectmenu).waitFor({ state: 'visible', timeout: 10000 });
   await page.locator(selectors.Selectmenu).scrollIntoViewIfNeeded({ timeout: 10000 });
   await page.click(selectors.Selectmenu);
-  await page
-    .locator(selectors.selectvalue)
-    .selectOption({ label: "Group 2, option 1" });
 
-  await page.locator(selectors.selectone).selectOption({ label: "Dr." });
+  await page.locator(selectors.selectvalue).click();
+  await page
+    .locator('//div[contains(@id, "react-select") and text()="Group 2, option 1"]')
+    .click();
+
+  await page.locator(selectors.selectone).click();
+  await page
+    .locator('//div[contains(@id, "react-select") and text()="Dr."]')
+    .click();
 
   await page
     .locator(selectors.oldstyleselectmenu)
     .selectOption({ label: "Green" });
 
+  await page.locator(selectors.multiplemenu).click();
   await page
-    .locator(selectors.multiplemenu)
-    .selectOption([{ label: "Green" }, { label: "Blue" }]);
+    .locator(selectors.selectblue)
+    .click();
+  await page.click(selectors.multiplemenu);
+ await page.click(selectors.multiplemenu);
+  await page
+    .locator(selectors.selectblue)
+    .click();
+
   await page.locator(selectors.standart).selectOption({ label: "Volvo" });
 }
